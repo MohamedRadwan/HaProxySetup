f99155629f6c578a73ca5876ee576e56e5e6beae
Mohamed Radwan <mohamed.rad@outlook.com>
2019-07-28 22:33:39 -0400

    set up proxy

--- setup.sh
+++ setup.sh
@@ -1,19 +1,9 @@
  docker create --name proxy \
-	-v  \
-    -v :/etc/ssl/private \
+	-v /root/web-HaProxy/haproxy.cfg:/usr/local/etc/haproxy/haproxy.cfg:ro \
+    -v /etc/letsencrypt/live/www.mradwan.tk:/etc/ssl/private:ro \
     -p 80:80 \
     -p 443:443 \
     haproxy:1.7
-
-docker run --detach \
-    --publish 80:80 \
-    --publish 443:443 \
-    --name proxycont \
-    --restart always \
-    --volume //root/web-HaProxy/haproxy.cfg:/usr/local/etc/haproxy/haproxy.cfg:ro \
-    --volume /etc/letsencrypt/live/www.mradwan.tk:/certs:ro \
-    haproxy:1.7
-
 
 
 docker run --name app-main \
