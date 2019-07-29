799fd3cf16a4d5f760cb253624eccfa88de7022b
Mohamed Radwan <mohamed.rad@outlook.com>
2019-07-28 22:16:05 -0400

    moved fiel

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
  docker create --name proxy \
 	-v /root/web-HaProxy/haproxy.cfg:/usr/local/etc/haproxy/haproxy.cfg:ro \
+    -v /etc/letsencrypt/live/www.mradwan.tk/cert.pem:/etc/ssl/private/cert.pem:ro \
     -p 80:80 \
     -p 443:443 \
     haproxy:1.7
