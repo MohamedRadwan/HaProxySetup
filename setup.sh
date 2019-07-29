bb08348a7f559677b069fd6eea72d792700065b0
Mohamed Radwan <mohamed.rad@outlook.com>
2019-07-28 22:50:13 -0400

    test

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
  docker create --name proxy \
 	-v /root/web-HaProxy/haproxy.cfg:/usr/local/etc/haproxy/haproxy.cfg:ro \
-    -v /etc/letsencrypt/live/www.mradwan.tk:/etc/ssl/private:ro \
     -p 80:80 \
     -p 443:443 \
     haproxy:1.7
@@ -10,3 +9,5 @@ docker run --name app-main \
  	-p 81:80 \
 	-v /root/web-personalwebsite:/usr/share/nginx/html \
 	-d nginx
+
+    /etc/letsencrypt/live/www.mradwan.tk
