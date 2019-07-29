6b834249a75dc3a43b97da4c5533d09aadf90331
Mohamed Radwan <mohamed.rad@outlook.com>
2019-07-28 22:12:39 -0400

    add ssl

---
+++ setup.sh
@@ -0,0 +1,10 @@
+ docker create --name proxy \
+	-v /root/web-HaProxy/haproxy.cfg:/usr/local/etc/haproxy/haproxy.cfg:ro \
+    -p 80:80 \
+    -p 443:443 \
+    haproxy:1.7
+
+docker run --name app-main \
+ 	-p 81:80 \
+	-v /root/web-personalwebsite:/usr/share/nginx/html \
+	-d nginx
