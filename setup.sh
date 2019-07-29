8e7386f24d2bebdcb1d32f14c786b168004060a4
Mohamed Radwan <mohamed.rad@outlook.com>
2019-07-28 22:26:12 -0400

    simple porx test

--- setup.sh
+++ setup.sh
@@ -1,9 +1,20 @@
  docker create --name proxy \
-	-v /root/web-HaProxy/haproxy.cfg:/usr/local/etc/haproxy/haproxy.cfg:ro \
-    -v /etc/letsencrypt/live/www.mradwan.tk/cert.pem:/etc/ssl/private/cert.pem:ro \
+	-v  \
+    -v :/etc/ssl/private \
     -p 80:80 \
     -p 443:443 \
     haproxy:1.7
+
+docker run --detach \
+    --publish 80:80 \
+    --publish 443:443 \
+    --name proxycont \
+    --restart always \
+    --volume //root/web-HaProxy/haproxy.cfg:/usr/local/etc/haproxy/haproxy.cfg:ro \
+    --volume /etc/letsencrypt/live/www.mradwan.tk:/certs:ro \
+    haproxy:1.7
+
+
 
 docker run --name app-main \
  	-p 81:80 \
