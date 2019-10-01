39b9253d00b747fa48fb88945b19fe066a6f0e64
Mohamed Radwan <mohamed.rad@outlook.com>
2019-09-30 20:41:01 -0400

    update

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
  docker create --name proxy2 \
 	-v /root/web-HaProxy/haproxy.cfg:/usr/local/etc/haproxy/haproxy.cfg:ro \
-    -v /etc/letsencrypt/live/www.mradwan.tk/all.pem:/etc/ssl/certs/all.pem:ro \
+    -v /etc/letsencrypt/live/mradwan.tk/all.pem:/etc/ssl/certs/all.pem:ro \
     -p 80:80 \
     -p 443:443 \
     haproxy:1.7
