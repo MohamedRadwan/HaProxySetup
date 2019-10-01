888c31800508bcc63229ee5460556a8a356e91ec
Mohamed Radwan <mohamed.rad@outlook.com>
2019-09-30 20:51:45 -0400

    fixes

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
- docker create --name proxy2 \
+ docker create --name proxy \
 	-v /root/web-HaProxy/haproxy.cfg:/usr/local/etc/haproxy/haproxy.cfg:ro \
     -v /etc/letsencrypt/live/mradwan.tk/all.pem:/etc/ssl/certs/all.pem:ro \
+	-v /root/web-HaProxy/errors:/etc/haproxy/errors-custom:ro \
     -p 80:80 \
     -p 443:443 \
     haproxy:1.7
